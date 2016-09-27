a247b940853546f83e6d1c0365e9cde0b68cef64
Joshua Goldmeier <joshuaegoldmeier@gmail.com>
2016-09-27 16:31:47 -0400

    added some more stuff...

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 var express = require('express');
+const volleyball = require('volleyball')
+
+
 
 var app = express();
 
+app.use(volleyball)
+
+app.use('/', function(request, response, next){
+	console.log(request.method);
+	next();
+})
+
+app.use('/special/', function(request, response, next){
+	console.log("you have reached special area");
+	next();
+})
+
 app.get('/', function(request, response){
 	response.send('loading...')
+})
+
+app.get('/news', function(request, response){
+	response.send('There is no important news today')
 })
 
 app.listen(3000, function(){
