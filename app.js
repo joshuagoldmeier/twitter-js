45d7049711ae77b005ac5639617a99e553cad36a
Joshua Goldmeier <joshuaegoldmeier@gmail.com>
2016-09-28 10:54:43 -0400

    Added a lot.  Still needa fix css rendering

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
 var express = require('express');
 const volleyball = require('volleyball')
+var nunjucks = require('nunjucks')
+var routes = require('./routes/');
 
-
+nunjucks.configure('views'); // point nunjucks to the proper directory for templates
 
 var app = express();
 
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+
 app.use(volleyball)
-
-app.use('/', function(request, response, next){
-	console.log(request.method);
-	next();
-})
-
-app.use('/special/', function(request, response, next){
-	console.log("you have reached special area");
-	next();
-})
-
-app.get('/', function(request, response){
-	response.send('loading...')
-})
-
-app.get('/news', function(request, response){
-	response.send('There is no important news today')
-})
+app.use('/', routes);
+app.use('/static', express.static(__dirname + '/public'));
 
 app.listen(3000, function(){
 	console.log('Were hooked in!')
 });
+
+
+// app.get('/', function(request, response){
+// 	//response.send('loading...')
+// 	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+// 	response.render( 'index', {title: 'Hall of Fame', people: people} );
+// })
---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+// could use one line instead: var router = require('express').Router();
+var tweetBank = require('../tweetBank');
+
+router.get('/', function (req, res) {
+  var tweets = tweetBank.list();
+  res.render( 'index', { tweets: tweets } );
+});
+
+router.get('/stylesheets/style.css', function (req, res) {
+  
+  res.sendFile('twitter-js/public/stylesheets/style.css')
+  //res.sendFile(path.join(__dirname, './public/stylesheets/style.css'));
+  
+});
+
+
+
+module.exports = router;
